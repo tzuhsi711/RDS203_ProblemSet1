9502cb06ab2a6777cb8947cb012c3a2fe6eaadda
tzuhsi711 <130109589+tzuhsi711@users.noreply.github.com>
2026-04-09 22:11:16 -0400

    Update Q2f.java

--- src/ps2/Q2f.java
+++ src/ps2/Q2f.java
@@ -6,12 +6,12 @@ public class Q2f {
                                       double S, double I, double R,
                                       double N, double h) {
 
-        // --- k1 ---
+        // k1
         double dS1 = -Q2a.newCases(beta, S, I, N);
         double dI1 = Q2a.newCases(beta, S, I, N) - Q2a.recoveredCases(gamma, I);
         double dR1 = Q2a.recoveredCases(gamma, I);
 
-        // --- k2 ---
+        // k2
         double S2 = S + h * dS1 / 2;
         double I2 = I + h * dI1 / 2;
 
@@ -19,7 +19,7 @@ public class Q2f {
         double dI2 = Q2a.newCases(beta, S2, I2, N) - Q2a.recoveredCases(gamma, I2);
         double dR2 = Q2a.recoveredCases(gamma, I2);
 
-        // --- k3 ---
+        // k3
         double S3 = S + h * dS2 / 2;
         double I3 = I + h * dI2 / 2;
 
@@ -27,7 +27,7 @@ public class Q2f {
         double dI3 = Q2a.newCases(beta, S3, I3, N) - Q2a.recoveredCases(gamma, I3);
         double dR3 = Q2a.recoveredCases(gamma, I3);
 
-        // --- k4 ---
+        // k4
         double S4 = S + h * dS3;
         double I4 = I + h * dI3;
 
@@ -35,7 +35,7 @@ public class Q2f {
         double dI4 = Q2a.newCases(beta, S4, I4, N) - Q2a.recoveredCases(gamma, I4);
         double dR4 = Q2a.recoveredCases(gamma, I4);
 
-        // --- final ---
+        // final
         double S_next = S + (h / 6) * (dS1 + 2*dS2 + 2*dS3 + dS4);
         double I_next = I + (h / 6) * (dI1 + 2*dI2 + 2*dI3 + dI4);
         double R_next = R + (h / 6) * (dR1 + 2*dR2 + 2*dR3 + dR4);
